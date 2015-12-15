c90a9387fd17245749581b09170800219f841e48
kveder <sarunas.kvederys@gmail.com>
2015-12-14 21:22:54 -0500

    added js for sitelink hidding, styling

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 $(function () {
+
+	$('#sitelink-select').change(function() {
+   	$('.sitelink').hide();
+   	$('.' + $(this).val()).show();
+	});
+
 	$('#input-headline').keyup(function() {
 		$('.ad-headline').text($(this).val());
 	});
-});
 
-$(function () {
 	$('#input-display-url').keyup(function() {
 		$('.ad-display-url').text($(this).val());
 	});
-});
 
-$(function () {
 	$('#input-desc-1').keyup(function() {
 		$('.ad-desc-1').text($(this).val());
 	});
-});
 
-$(function () {
-	$('#input-desc-2').keyup(function() {
-		$('.ad-desc-2').text($(this).val());
+	$('#input-sitelink-headline-1').keyup(function() {
+		$('#sitelink-headline-1').text($(this).val());
+	});
+
+	$('#input-sitelink-desc-1-1').keyup(function() {
+		$('#sitelink-desc-1-1').text($(this).val());
+	});
+	$('#input-sitelink-desc-2-1').keyup(function() {
+		$('#sitelink-desc-2-1').text($(this).val());
 	});
 });
 
+
+
+
+// http://stackoverflow.com/questions/2777139/how-to-use-jquery-to-show-hide-divs-based-on-radio-button-selection
