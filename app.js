d51f8a42936c7ebe0b56ef834048fe7b1ce2e836
kveder <sarunas.kvederys@gmail.com>
2015-12-15 00:15:32 -0500

    added more styles, forms, background.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 $(function () {
 
-	$('#sitelink-select').change(function() {
-   	$('.sitelink').hide();
-   	$('.' + $(this).val()).show();
-	});
+	 $("input[name=sitelink-status]").on("change",function() {
+         var test = $(this).val();
+         $(".hide-sitelinks").hide();
+         $("."+test).show();
+    });
+
 
 	$('#input-headline').keyup(function() {
 		$('.ad-headline').text($(this).val());
@@ -16,6 +18,10 @@ $(function () {
 	$('#input-desc-1').keyup(function() {
 		$('.ad-desc-1').text($(this).val());
 	});
+	
+	$('#input-desc-2').keyup(function() {
+		$('.ad-desc-2').text($(this).val());
+	});
 
 	$('#input-sitelink-headline-1').keyup(function() {
 		$('#sitelink-headline-1').text($(this).val());
@@ -27,9 +33,11 @@ $(function () {
 	$('#input-sitelink-desc-2-1').keyup(function() {
 		$('#sitelink-desc-2-1').text($(this).val());
 	});
+
+	    
 });
 
 
-
-
+// Learning material:
 // http://stackoverflow.com/questions/2777139/how-to-use-jquery-to-show-hide-divs-based-on-radio-button-selection
+// http://tympanus.net/codrops/2010/06/07/fancy-sliding-form-with-jquery/
